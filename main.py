a1a236d9b901f8ab05cd0947f4ad4ed947193520
kerem212012 <rockstarfoxykerem@gmail.com>
2024-10-19 12:29:08 +0300

    space

--- main.py
+++ main.py
@@ -3,44 +3,46 @@ def cipher(mode, language, shift, letter):
     english = ' '.join('abcdefghijklmnopqrstuvwxyz').split()
     russian = ' '.join('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ').lower().split()
     if language == 'english':
-       if mode == 'cipher':
-           numletter = len(letter)
-           for i in range(numletter):
+        if mode == 'cipher':
+            numletter = len(letter)
+            for i in range(numletter):
                 letter = ' '.join(letter).split()
-                ind =english.index(letter[i])+shift
+                ind = english.index(letter[i]) + shift
                 ind = ind % 26
                 del letter[i]
-                letter.insert(i,english[ind])
+                letter.insert(i, english[ind])
                 letter = ''.join(letter)
-       elif mode == 'decipher':
-           numletter = len(letter)
-           for j in range(numletter):
+        elif mode == 'decipher':
+            numletter = len(letter)
+            for j in range(numletter):
                 letter = ' '.join(letter).split()
-                ind =english.index(letter[j])+(shift*-1)
+                ind = english.index(letter[j]) + (shift * -1)
                 ind = ind % 26
                 del letter[j]
-                letter.insert(j,english[ind])
+                letter.insert(j, english[ind])
                 letter = ''.join(letter)
     elif language == 'russian':
-       if mode == 'cipher':
-           numletter = len(letter)
-           for k in range(numletter):
+        if mode == 'cipher':
+            numletter = len(letter)
+            for k in range(numletter):
                 letter = ' '.join(letter).split()
-                ind =russian.index(letter[k])+shift
+                ind = russian.index(letter[k]) + shift
                 ind = ind % 26
                 del letter[k]
-                letter.insert(k,russian[ind])
+                letter.insert(k, russian[ind])
                 letter = ''.join(letter)
-       elif mode == 'decipher':
-           numletter = len(letter)
-           for l in range(numletter):
+        elif mode == 'decipher':
+            numletter = len(letter)
+            for l in range(numletter):
                 letter = ' '.join(letter).split()
-                ind =russian.index(letter[l])+(shift*-1)
+                ind = russian.index(letter[l]) + (shift * -1)
                 ind = ind % 26
                 del letter[l]
-                letter.insert(l,russian[ind])
+                letter.insert(l, russian[ind])
                 letter = ''.join(letter)
-    print('Your word:',letter)
+    print('Your word:', letter)
+
+
 def main():
     print('Select mode (cipher/decipher)')
     mode = input().strip()
